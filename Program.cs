c75213040f95bf79fee0e55ebbbb373bd2cb069a
carlos_costa <carloscosta.pen@gmail.com>
2026-03-20 21:17:24 -0300

    adding parameterized fun

--- Program.cs
+++ Program.cs
@@ -41,11 +41,57 @@ namespace DBConnection
             }
             catch(Exception ex) { 
                 Console.WriteLine("Error: " + ex.Message);
+                Connection.Close();
+
             }
         }
+        //Prameterized Query
+        static void GetPorscheCars(string CarName)
+        {
+            SqlConnection Connection = new SqlConnection(ConnectionString);
+            string Query = "select top 5 Vehicle_Display_Name, Engine_CC, Model from CarsDataBase" +
+                " where Vehicle_Display_Name like @CarName;";
+            SqlCommand Command = new SqlCommand(Query, Connection);
+            Command.Parameters.AddWithValue("@CarName", "%" + CarName + "%");
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    string Name = (string)Reader["Vehicle_Display_Name"];
+                    string Model = (string)Reader["Model"];
+                    short EngineCC = (short)Reader["Engine_CC"];
+
+                    Console.WriteLine($"Name: {Name}");
+                    Console.WriteLine($"Model: {Model}");
+                    Console.WriteLine($"Engine CC:{EngineCC}");
+                    Console.WriteLine();
+
+
+
+                }
+
+
+                Reader.Close();
+                Connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Connection.Close();
+
+            }
+        }
+
         static void Main(string[] args)
         {
-            PrintCarsDetails();
+            //PrintCarsDetails();
+            GetPorscheCars("Porsche%");
+
             Console.ReadKey();  
         }
     }
